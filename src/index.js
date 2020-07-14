b99805c94964e4978fbfd3121d6e86d7382609d2
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-07-14 09:50:16 -0500

    test run successful

--- src/index.js
+++ src/index.js
@@ -3,4 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types';
 // var PropTypes = require('prop-types'); // ES5 with npm
-import Big from './big.mjs';
+// import Big from './big.mjs';
+const Big = require('big.js');
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 1,
+    };
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1> Test result: {this.state.test} </h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Test />,
+  document.getElementById('root')
+)
