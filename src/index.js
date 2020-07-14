9081f1d558fd4edcaabfe0e257674f53f4f3e1ca
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-07-14 09:41:07 -0500

    big.js and prop-types add

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types';
+// var PropTypes = require('prop-types'); // ES5 with npm
+import Big from './big.mjs';
