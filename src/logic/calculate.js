5cfbafee2baa4508e928f144f1f8580e89e9ef61
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-07-27 20:08:47 -0500

    logic added

---
+++ src/logic/calculate.js
@@ -0,0 +1,104 @@
+import Big from 'big.js';
+import operate from './operate';
+
+const calculate = (data, button) => {
+  let {total, next, operation} = data;
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+  const isNumber = numbers.includes(button);
+  const operations = ['+', '-', 'X', '%', '+/-', '÷'];
+  const isOperation = operations.includes(button);
+  const eq = ['='].includes(button);
+
+  if (button = 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+    return { next,
+      total,
+      operation
+    };
+  }
+
+  if (eq && total && next && operation) {
+    total = operate(total, next, operation);
+    next = null;
+    operation = null;
+    return { next,
+      total,
+      operation
+    };
+  }
+
+  if (numbers && total && !next && !operation) {
+    next = button;
+    total = null;
+    operation = null;
+    return {
+      next,
+      total,
+      operation
+    };
+  }
+
+  if (isNumber) {
+    if (total === null && next === null && button === '0') {
+      return { next, total, operation };
+    }
+    next = next === null ? button : next += button;
+    return { next, total, operation };
+  }
+
+  if (isOperation && next && button === '+/-') {
+    next = (parseFloat(next) * -1).toString();
+    return { next, total, operation };
+  }
+
+  if (isOperation && total && !next && !operation && button === '+/-') {
+    next = (parseFloat(total) * -1).toString();
+    total = null;
+    return { next, total, operation };
+  }
+
+  if (isOperation && total && button === '+/-') {
+    next = (parseFloat(total) * -1).toString();
+    return { next, total, operation };
+  }
+
+  if (isOperation && next && button === '%') {
+    next = (parseFloat(next) / 100).toString();
+    return { next, total, operation };
+  }
+
+  if (isOperation && total && !next && !operation && button === '%') {
+    total = (parseFloat(total) / 100).toString();
+    return { next, total, operation };
+  }
+
+  if (isOperation && total && button === '%') {
+    next = (parseFloat(next) / 100).toString();
+    return { next, total, operation };
+  }
+
+  if (isOperation && next && total && operation) {
+    next = operate(total, next, operation);
+    total = null;
+    operation = null;
+    return { next, total, operation };
+  }
+
+  if (operations && total) {
+    operation = button;
+    return { next, total, operation };
+  }
+
+  if (operations && button !== '+/-' && next) {
+    total = next;
+    next = null;
+    operation = button;
+    return { next, total, operation };
+  }
+
+  return { next, total, operation };
+};
+
+export default calculate;
---
+++ src/logic/operate.js
@@ -0,0 +1,32 @@
+import Big from 'big.js';
+
+const operate = (no1, no2, op) => {
+  let result = Big(0);
+  const number1 = Big(no1);
+  const number2 = Big(no2);
+  switch (op) {
+    case '+':
+      result = number1.plus(number2).toString();
+      break;
+    case '-':
+      result = number1.minus(number2).toString();
+      break;
+    case '*':
+      result = number1.times(number2).toString();
+      break;
+    case '/':
+      if (number2.toString() === '0') {
+        result = 'ERROR';
+      } else {
+        result = number1.div(number2).toString();
+      }
+      break;
+    case '%':
+      result = number1.div(100).times(number2).toString();
+      break;
+    default:
+      return result;
+  }
+};
+
+export default operate;
