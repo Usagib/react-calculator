3d5694d60c5c53176b51ac811b863f1383285eca
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-07-27 20:44:42 -0500

    add state to app component and handleclickfunction

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,31 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate'; // eslint-disable-line no-unused-vars
 import '../index.css';
 
-export default function App() {
-  return (
-    <div id="Calcualtor">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    this.setState({
+      total: state.total,
+      next: state.next,
+      operation: state.operation,
+    })
+  }
+  
+  render() {
+    return (
+      <div id="Calcualtor">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+
 }
